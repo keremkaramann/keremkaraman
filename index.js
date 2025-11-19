030823d39f89ab99b801788adb437fd02c7eeb63
Kerem Karaman <keremkaraman91@gmail.com>
2025-11-19 21:04:35 +0300

    info section added

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+//Variables
 const seesaw_plank = document.querySelector(".seesaw-plank");
 const seesaw_cont = document.querySelector(".seesaw-container-2");
 const leftValue = document.querySelector("#left_weight");
 const rightValue = document.querySelector("#right_weight");
+const tiltAngle = document.querySelector("#tilt_angle");
+const infoSection = document.querySelector(".info-section");
 leftValue.textContent = 0;
 rightValue.textContent = 0;
+tiltAngle.textContent = 0;
 
 const leftWeights = [];
 const rightWeights = [];
 
+//Whole logic
 seesaw_cont.addEventListener("click", (e) => {
   const weight = Math.floor(Math.random() * 10) + 1;
   const size = weight * 5;
@@ -21,6 +26,8 @@ seesaw_cont.addEventListener("click", (e) => {
   } else {
     rightWeights.push(weight);
   }
+  let side = clickX < middle ? "left" : "right";
+  let distance = Math.abs(clickX - middle);
 
   const sumLeft = leftWeights.reduce((a, b) => a + b, 0);
   leftValue.textContent = sumLeft;
@@ -29,8 +36,9 @@ seesaw_cont.addEventListener("click", (e) => {
   rightValue.textContent = sumRight;
 
   let diff = sumRight - sumLeft;
-  diff = Math.max(-15, Math.min(15, diff));
+  diff = Math.max(-30, Math.min(30, diff));
   seesaw_plank.style.transform = `translateX(-50%) rotate(${diff}deg)`;
+  tiltAngle.textContent = `${diff}.0`;
 
   const newWeight = document.createElement("div");
   let red = Math.floor(Math.random() * 250 + 0);
@@ -49,11 +57,21 @@ seesaw_cont.addEventListener("click", (e) => {
 
   seesaw_plank.appendChild(newWeight);
 
+  //info section
+  const p = document.createElement("p");
+  const icon = document.createElement("i");
+  icon.className = "fa-solid fa-dumbbell";
+
+  p.appendChild(icon);
+  p.append(` ${weight} dropped on ${side} side at ${distance}px from center`);
+  infoSection.prepend(p);
+
   setTimeout(() => {
     newWeight.style.transform = "translateY(0)";
   }, 50);
 });
 
+//Reset Logic
 function resetFunc() {
   leftWeights.length = 0;
   rightWeights.length = 0;
