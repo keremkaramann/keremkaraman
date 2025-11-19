6c9b9d44586fa659247f020f152eab92c30d4428
Kerem Karaman <keremkaraman91@gmail.com>
2025-11-19 16:42:00 +0300

    weight html element created

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const seesaw_plank = document.querySelector(".seesaw-plank");
 const seesaw_cont = document.querySelector(".seesaw-container-2");
-const weight = Math.floor(Math.random() * 10) + 1;
-console.log(seesaw_plank);
-console.log(weight);
 
 seesaw_cont.addEventListener("click", (e) => {
-  console.log("Çalışıyor");
+  const weight = Math.floor(Math.random() * 10) + 1;
+  const size = weight * 5;
+
+  const newWeight = document.createElement("div");
+  let red = Math.floor(Math.random() * 250 + 0);
+  let green = Math.floor(Math.random() * 250 + 0);
+  let blue = Math.floor(Math.random() * 250 + 0);
+  let rgb = "rgb(" + red + "," + green + "," + blue + ")";
+  newWeight.className = "weight";
+  newWeight.style.width = 20 + size + "px";
+  newWeight.style.height = 20 + size + "px";
+  newWeight.textContent = weight;
+  newWeight.style.backgroundColor = rgb;
+  newWeight.style.color = "white";
+  seesaw_plank.appendChild(newWeight);
 });
